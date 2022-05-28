cb10a933d6da00a5c8581fd978f755c8413cf10f
swiches <donasare@live.com>
2022-05-28 15:29:17 +0000

    Refactored Routes and Added DB.json

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -4,21 +4,17 @@ const workoutController = require("../../controllers/workoutController")
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Get all workouts");
-});
+router.get("/", workoutController.getAllWorkouts );
+ 
 
-router.get("/:workoutId", (req, res) =>{
-    res.send("Get an existing workout");
-} );
+router.get("/:workoutId", workoutController.getOneWorkout);
 
-router.post("/", (req, res) => {
-    res.send("Create a new workout");
-});
 
-router.patch("/:workoutId", (req, res) => {
-    res.send("Update an existing workout");
-});
+router.post("/", workoutController.createNewWorkout);
+
+
+router.patch("/:workoutId", workoutController.updateOneWorkout);
+
 
 router.delete("/:workoutId", workoutController.deleteOneWorkout);
 
