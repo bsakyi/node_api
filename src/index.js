89d5fad6d68fd3b440dfe5f94ec6c6f27b5844fa
swiches <donasare@live.com>
2022-05-31 12:22:13 +0000

    Added Body Parser

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser =  require("body-parser");
 
 // const v1Router = require("./v1/routes");
 
@@ -6,6 +7,9 @@ const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use("/api/v1/workouts", v1WorkoutRouter);
 
 
 //for testing purposes
